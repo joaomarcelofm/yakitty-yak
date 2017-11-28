7b5f5b0befff9c1dca4311e00cc518e70df1969b
Aleeya Dasgupta <aleeya@gmail.com>
2017-11-28 13:03:34 +0100

    added scope to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  provider :slack, 'API_KEY', 'API_SECRET', scope: 'identity.basic', name: :sign_in_with_slack
+  provider :slack, 'API_KEY', 'API_SECRET', scope: 'team:read,users:read,identify,bot'
 end
