f0dfb9f01a86038e0da903bcc619aa834eb82d27
Joao Martins <joaomarcelofm@gmail.com>
2017-11-28 17:49:02 +0100

    almost there

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,15 +4,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Slack") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Slack") if is_navigational_format?
     else
       session["devise.slack_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
 
-  def failure
+  def failurefrom_omniauth
     redirect_to root_path
   end
 end
