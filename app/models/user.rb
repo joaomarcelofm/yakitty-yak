a2ae58810fe80418b60e14da108e1daeae177005
Aleeya Dasgupta <aleeya@gmail.com>
2017-11-28 13:17:15 +0100

    added scopes to devise.rb, removed from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  provider :slack, 'API_KEY', 'API_SECRET', scope: 'identity.basic', name: :sign_in_with_slack
-  provider :slack, 'API_KEY', 'API_SECRET', scope: 'team:read,users:read,identify,bot'
+
 end
