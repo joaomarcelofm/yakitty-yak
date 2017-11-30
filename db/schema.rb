526e7347f9a0cae7ee8554e03876dd2f9de70f8c
Joao Martins <joaomarcelofm@gmail.com>
2017-11-30 16:50:51 +0100

    removed the date and time from meetings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130114258) do
+ActiveRecord::Schema.define(version: 20171130154903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20171130114258) do
   end
 
   create_table "meetings", force: :cascade do |t|
-    t.date "date"
-    t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "room_name"
