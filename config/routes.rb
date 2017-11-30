1279d0f90c7b23082d4c12e032917d76cdee3b2a
Joao Martins <joaomarcelofm@gmail.com>
2017-11-30 18:50:59 +0100

    form

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", users: "users/users" }
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resources :requests, only: [:new, :create]
+  end
 
   root to: 'pages#home'
 
   get '/form', to: 'pages#form'
   get '/meetings', to: "pages#meetings"
 
-  resources :meetings do
-    resources :meetings, only: [:new, :show]
-  end
+  resources :meetings, only: [:new, :show]
 
 end
