bd9aed54eb5ca62644611d207edc765e42ea1688
Jared Guyer <jaredg8@gmail.com>
2017-12-01 15:56:36 +0100

    updated requests

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,11 @@
 class Skill < ApplicationRecord
   has_many :user_skills
   has_many :users, through: :user_skills
+
+
+  def self.skill_match(skillname)
+    skill = Skill.find_by(name: skillname)
+    user_list = skill.users
+    return user_list
+  end
 end
