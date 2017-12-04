e99e7702d0cdd7ca2281dbd55da925da2e60f5f5
Jared Guyer <jaredg8@gmail.com>
2017-12-04 17:24:58 +0100

    adding updates to slack message

--- app/models/request.rb
+++ app/models/request.rb
@@ -15,8 +15,38 @@ class Request < ApplicationRecord
 
   def slack_request
     client = Slack::Web::Client.new
-    client.chat_postMessage(channel: receiver.uid, text: "Hello #{receiver.name}! You have recieved a new request from #{user.name}.  Because '#{Request.last.skill}' is a skill of yours, they would like to discuss '#{Request.last.topic}' with you.  They have requested to meet at #{Request.last.start_time.strftime("%a, %d %b")}")
+    client.chat_postMessage(
+      channel: receiver.uid,
+      text: "Hello #{receiver.name} :wave: :speech_balloon:!",
+      username: "Yakitty-Yak",
+      attachments: [
+            {
+                fallback: "XYZ",
+                pretext: "Incoming from _Yakitty-Yak_ ",
+                title: "Chat Request",
+                title_link: "http://localhost:3000/meetings/#{Meeting.last.id}",
+                text: "You have received a new request from #{user.name}. Because '#{Request.last.skill}' is a skill of yours, they would like to discuss '#{Request.last.topic}' with you.  \nThey have requested to meet at #{Request.last.start_time.strftime("%a, %d %b")}.",
+                color: "#38B684",
+                actions: [
+                {
+                    "name": "accept",
+                    "text": "Accept",
+                    "style": "primary",
+                    "type": "button",
+                    "value": "accept"
+                },
+                {
+                    "name": "reject",
+                    "text": "Reject",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "reject"
+                }
+            ]
+
+            }
+        ].to_json
+        )
   end
-
-
 end
+
